e644f48bd4932d99d996bcfab694a97dcb34b9ef
MaksimMyson <maksimmyson33@gmail.com>
2024-03-15 19:24:32 +0200

    factoru metod was done

--- CSharp8_Patern_1_05_03/Program.cs
+++ CSharp8_Patern_1_05_03/Program.cs
@@ -1,10 +1,61 @@
 ﻿namespace CSharp8_Patern_1_05_03
 {
-    internal class Program
+    abstract class Product
+    {
+        public abstract void Display();
+    }
+
+    
+    class ConcreteProductA : Product
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Concrete Product A");
+        }
+    }
+
+    class ConcreteProductB : Product
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Concrete Product B");
+        }
+    }
+
+
+    abstract class Creator
+    {
+        public abstract Product FactoryMethod();
+    }
+
+
+    class ConcreteCreatorA : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductA();
+        }
+    }
+
+    class ConcreteCreatorB : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductB();
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Creator creatorA = new ConcreteCreatorA();
+            Product productA = creatorA.FactoryMethod();
+            productA.Display();
+
+            Creator creatorB = new ConcreteCreatorB();
+            Product productB = creatorB.FactoryMethod();
+            productB.Display();
         }
     }
 }
