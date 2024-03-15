aee34287b9b6c31512902a8a0673e60da5c786bc
MaksimMyson <maksimmyson33@gmail.com>
2024-03-15 19:41:50 +0200

    builder was done

--- CSharp8_Patern_1_05_03/Program.cs
+++ CSharp8_Patern_1_05_03/Program.cs
@@ -1,10 +1,101 @@
 ﻿namespace CSharp8_Patern_1_05_03
 {
-    internal class Program
+    class Pizza
+    {
+        public string Dough { get; set; } = "default dough";
+        public string Sauce { get; set; } = "default sauce";
+        public string Topping { get; set; } = "default topping";
+
+        public void Display()
+        {
+            Console.WriteLine($"Pizza with {Dough} dough, {Sauce} sauce, and {Topping} topping.");
+        }
+    }
+
+    abstract class PizzaBuilder
+    {
+        protected Pizza pizza = new Pizza();
+
+        public Pizza GetPizza()
+        {
+            return pizza;
+        }
+
+        public abstract void BuildDough();
+        public abstract void BuildSauce();
+        public abstract void BuildTopping();
+    }
+
+    class MargheritaPizzaBuilder : PizzaBuilder
+    {
+        public override void BuildDough()
+        {
+            pizza.Dough = "thin crust";
+        }
+
+        public override void BuildSauce()
+        {
+            pizza.Sauce = "tomato";
+        }
+
+        public override void BuildTopping()
+        {
+            pizza.Topping = "mozzarella cheese";
+        }
+    }
+
+    class PepperoniPizzaBuilder : PizzaBuilder
+    {
+        public override void BuildDough()
+        {
+            pizza.Dough = "thick crust";
+        }
+
+        public override void BuildSauce()
+        {
+            pizza.Sauce = "spicy tomato";
+        }
+
+        public override void BuildTopping()
+        {
+            pizza.Topping = "pepperoni slices";
+        }
+    }
+
+    class PizzaDirector
+    {
+        private readonly PizzaBuilder pizzaBuilder;
+
+        public PizzaDirector(PizzaBuilder builder)
+        {
+            pizzaBuilder = builder;
+        }
+
+        public void MakePizza()
+        {
+            pizzaBuilder.BuildDough();
+            pizzaBuilder.BuildSauce();
+            pizzaBuilder.BuildTopping();
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            PizzaBuilder margheritaBuilder = new MargheritaPizzaBuilder();
+            PizzaDirector margheritaDirector = new PizzaDirector(margheritaBuilder);
+            margheritaDirector.MakePizza();
+            Pizza margheritaPizza = margheritaBuilder.GetPizza();
+            margheritaPizza.Display();
+
+            PizzaBuilder pepperoniBuilder = new PepperoniPizzaBuilder();
+            PizzaDirector pepperoniDirector = new PizzaDirector(pepperoniBuilder);
+            pepperoniDirector.MakePizza();
+            Pizza pepperoniPizza = pepperoniBuilder.GetPizza();
+            pepperoniPizza.Display();
         }
     }
+
 }
+
