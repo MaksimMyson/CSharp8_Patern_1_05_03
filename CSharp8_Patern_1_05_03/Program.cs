9a4f4c9f72e931a496e8e06dcf0c1a239c6ee9aa
MaksimMyson <maksimmyson33@gmail.com>
2024-03-15 19:47:46 +0200

    prototype was done

--- CSharp8_Patern_1_05_03/Program.cs
+++ CSharp8_Patern_1_05_03/Program.cs
@@ -1,10 +1,49 @@
 ﻿namespace CSharp8_Patern_1_05_03
 {
-    internal class Program
+    abstract class Prototype
+    {
+        public abstract Prototype Clone();
+    }
+
+    
+    class ConcretePrototype : Prototype
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ConcretePrototype(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}";
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+           
+            ConcretePrototype original = new ConcretePrototype(1, "Original Object");
+            Console.WriteLine("Original object: " + original);
+
+           
+            ConcretePrototype cloned = (ConcretePrototype)original.Clone();
+            Console.WriteLine("Cloned object: " + cloned);
+
+            
+            cloned.Id = 2;
+            cloned.Name = "Cloned Object";
+            Console.WriteLine("Updated cloned object: " + cloned);
         }
     }
 }
