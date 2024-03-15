1efc5bf14117e0dd55c0ebdb9b3bc97dcbc12d93
MaksimMyson <maksimmyson33@gmail.com>
2024-03-15 20:00:08 +0200

    singleton was done

--- CSharp8_Patern_1_05_03/Program.cs
+++ CSharp8_Patern_1_05_03/Program.cs
@@ -1,10 +1,51 @@
 ﻿namespace CSharp8_Patern_1_05_03
 {
-    internal class Program
+    public class Singleton
+    {
+        
+        private static Singleton instance;
+
+        
+        private Singleton() { }
+
+       
+        public static Singleton GetInstance()
+        {
+            
+            if (instance == null)
+            {
+                instance = new Singleton(); 
+            }
+            return instance; 
+        }
+
+        
+        public void SomeMethod()
+        {
+            Console.WriteLine("Method of Singleton class is called.");
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            
+            Singleton instance1 = Singleton.GetInstance();
+            Singleton instance2 = Singleton.GetInstance();
+
+            
+            if (instance1 == instance2)
+            {
+                Console.WriteLine("Both instances are the same.");
+            }
+            else
+            {
+                Console.WriteLine("Instances are different.");
+            }
+
+            
+            instance1.SomeMethod();
         }
     }
 }
